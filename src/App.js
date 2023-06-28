0c8cb38343743c10033abda55aa76bc4487d3bbc
Steven Perez <stevenalp18@gmail.com>
2023-06-28 12:26:00 -0400

    added new features with local host

--- src/App.js
+++ src/App.js
@@ -6,30 +6,23 @@ import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
-import CampsiteDetail from "./features/campsites/CampsiteDetail";
+// import CampsiteDetail from "./features/campsites/CampsiteDetail";
 import "./App.css";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import AboutPage from "./pages/AboutPage";
 import { fetchCampsites } from "./features/campsites/campsitesSlice";
 import { fetchPartners } from "./features/partners/partnersSlice";
 import { fetchPromotions } from "./features/promotions/promotionsSlice";
+import { fetchComments } from "./features/comments/commentsSlice";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCampsites());
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(fetchCampsites());
-    dispatch(fetchPartners());
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(fetchCampsites());
     dispatch(fetchPartners());
     dispatch(fetchPromotions());
+    dispatch(fetchComments());
   }, [dispatch]);
 
   return (
